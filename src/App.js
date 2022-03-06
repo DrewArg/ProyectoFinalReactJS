9028b8a4da54e018d139e913166101d991cdcd4f
DrewArg <andresfabbiano5@gmail.com>
2022-03-05 22:53:59 -0300

    agregado h1 y fondo principal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
+import Titulo from './components/Titulo/Titulo';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+      <Titulo />
     </div>
   );
 }
---
+++ src/components/Titulo/Titulo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../Titulo/Titulo.css'
+
+const Titulo = () => {
+    return (
+        <><header>
+            <h1 className="titulo">Batalla por el Reino Animal</h1>
+            <div className="titulo__subtitulo">"La Batalla del Reino Animal" es un juego de cartas por turnos para dos
+                jugadores que está basado en "Mitos
+                y
+                Leyendas"</div>
+        </header>
+        </>
+    )
+}
+
+export default Titulo
